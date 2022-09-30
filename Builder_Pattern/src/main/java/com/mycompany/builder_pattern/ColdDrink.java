650ea75cebc8bd8295a8232ce10811a1aa0c4e6a
EdwinDutaRamadhan <89692738+EdwinDutaRamadhan@users.noreply.github.com>
2022-09-30 16:55:09 +0700

    Builder Pattern Java

---
+++ Builder_Pattern/src/main/java/com/mycompany/builder_pattern/ColdDrink.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.builder_pattern;
+
+/**
+ *
+ * @author HP
+ */
+public abstract class ColdDrink implements Item {
+    @Override
+    public Packing packing(){
+        return new Bottle();
+    }
+    
+    @Override
+    public abstract float price();
+}
---
+++ Builder_Pattern/src/main/java/com/mycompany/builder_pattern/Item.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.builder_pattern;
+
+/**
+ *
+ * @author HP
+ */
+public interface Item {
+    public String name();
+    public Packing packing();
+    public float price();
+}
